e33aa5e655cd77480cda89588750806eb0c770e5
sl-abreu <35570202+sl-abreu@users.noreply.github.com>
2021-03-13 18:50:36 -0600

    generaHijos completado, correcciones a astar, actulizacion de estructura de datos
    Falta costoF y probar algunas funciones.

--- aStar.lisp
+++ aStar.lisp
@@ -1,12 +1,20 @@
 (defun clear () (screen:clear-window (screen:make-window)))
 
 
-(setq abierto '(edoInicial) cerrado '() edoFinal '((1 2 3)(4 0 5)(6 7 8)))
+nodo=(ID padre f(x) g(x) movimiento (estado))
+
+movs=((1 0) (-1 0) (0 1) (0 -1))
+        4      3     2      1
+
+solucion=((mov padre) (mov padre) ...)
+
+
+(setq abierto (list nodoInicial) cerrado '() edoFinal '((1 2 3)(4 0 5)(6 7 8)))
 (setq solucion '())
-(setq movsDisp '(-2 -1 1 2))
-(setq id 1)
-(defun astarPuzzle8 (abierto cerrado edoFinal)
-    (let mejorNodo (car abierto))
+(setq movs '((1 0)(-1 0)(0 1)(0 -1)))
+(setq id 0)
+(defun astarPuzzle8 (abierto cerrado)
+    (setf mejorNodo (pop abierto))
     (cond 
         ((null mejorNodo) 
             (print 'NO_HAY_SOLUCION))
@@ -14,44 +22,47 @@
             (setq solucion (append solucion (list (list (cadddr mejorNodo) (cadr mejorNodo))))) (backtrack (reverse cerrado)))
         ((previo (car (cddddr mejorNodo) (cdr cerrado))) 
             (setq abierto (cdr abierto)) 
-            (astarPuzzle8 abierto cerrado edoFinal))
+            (astarPuzzle8 abierto cerrado))
         (t (generaHijos mejorNodo) 
             (setq cerrado (append cerrado (list mejorNodo)) abierto (cdr abierto)) 
             (sort abierto #'< :key #'third) 
-            (astarPuzzle8 abierto cerrado edoFinal))))
+            (astarPuzzle8 abierto cerrado))))
 
 ;dado un nodo se generan sus hijos y se agregan a abierto
-(defun generaHijos (nodo)
-    ;revisa cuantos hijos puede generar y checa eso 
+(defun generaHijos (nodo movs)
+    (cond 
+        ((null movs))
+        (t 
+            (let* ((edoP (cadr (cddddr nodo)))(edoH (generaEdo edoP (car movs)))(nodoH)(g (+ (nth 3 nodo) 1)))
+                (cond
+                    ((null edoH))
+                    (t (setf nodoH (list (+ id 1) (car nodo) (costoF g edoH) g (length movs) edoH)) (incf id) (push nodoH abierto))
+                )
+            )
+            (generaHijos nodo (cdr movs))
+        )
     )
+)
 
-;generar un nodo de la forma: nodo=(ID padre f(x) movimiento (estado))
+;calcula el costo de un estado (distancia de hamming o Brooklyn)
+(defun costoF (g edo)
+    -1)
 
-
-;genera un edo a partir del edoP y mov
+;genera un estado dado el padre y el mov a realizar, regresa nil si no es valido el mov
 (defun generaEdo (edoP mov &aux posA posD)
     ;arma que el nodo sea justo de la forma especifica y le mete todo lo que se necesita
     (setq edo (copy-tree edoP))
     (let* ((d0 (copy-tree '(0 0)))(posA (findij d0 edo))(posD (list (+ (car posA) (car mov)) (+ (cadr posA) (cadr mov)))))
     (cond
-        ((and (>= 3 (car posD) 1) (>= 3 (cadr posD) 1)) (write 'valido) (swap edo posA posD))
+        ((and (>= 3 (car posD) 1) (>= 3 (cadr posD) 1))(swap edo posA posD))
         (t nil)))
 )
-
-
-;dado un edo y un mov te dice si esta prohibido ese movimiento 
-(defun movProhib (edo mov)
-    (cond
-    ((eql mov 1) (miembro 0 (list (caar edo) (cadar edo) (caddar edo))))
-    ((eql mov -1) (miembro 0 (list (caaddr edo) (car (cdaddr edo))(cadr (cdaddr edo)) )))
-    ((eql mov 2)(miembro 0 (list (caddar edo) (car (cddadr edo))  (cadr (cdaddr edo)) )))
-    ((eql mov -2)(miembro 0 (list (caar edo) (caadr edo) (caaddr edo))))))
 
 ;recorre cerrado para obtener el conjunto de movimientos que llevan del estado inicial al estado final
 (defun backtrack (lst)
     (cond
         ((null lst) (print solucion))
-        ((eql (caar lst) (cadar (reverse solucion))) (setq solucion (append solucion (list (list (cadddr (car lst)) (cadr (car lst)))))) (backtrack (cdr lst)))
+        ((eql (caar lst) (cadar (reverse solucion))) (push (list (car (cddddr (car lst))) (cadr (car lst))) solucion) (backtrack (cdr lst)))
         (t (backtrack (cdr lst)))))
 
 ;regresa T si el estado pertenece al lst de nodos
@@ -59,14 +70,8 @@
     (setq nodo (car lst))
     (cond 
         ((null nodo) nil)
-        ((equal estado (car (cddddr nodo))))
+        ((equal estado (cadr (cddddr nodo))))
         (t (previo estado (cdr lst)))))
-
-nodo=(ID padre f(x) movimiento (estado))
-
-movs=((1 0) (-1 0) (0 1) (0 -1))
-
-solucion=((mov padre) (mov padre) ...)
 
 (defun miembro (obj lst)
     (cond
@@ -74,26 +79,6 @@ solucion=((mov padre) (mov padre) ...)
         ((equal obj (car lst)) t)
         (t (miembro obj (cdr lst)))))
 
-;edo ((1 2 3) (4 5 6) (7 8 9))
-#1 caar
-#2 cadar
-#3 caddar
-#4 caadr
-#5 cadadr
-#6 (car (cddadr edo))
-#7 caaddr
-#8  (car (cdaddr edo))
-#9  (cadr (cdaddr edo))
-
-
-;movimientos:
-;    1=sube al cero (1 2 3)
-;    -1=baja al cero (7 8 9)
-;    2=mueve a la derecha al cero (3 6 9)
-;    -2=mueve a la no derehca al cero (1 4 7)
-;
-
-(setq pos '(0 0))
 (defun findIJ (pos edo)
     (cond   
         ((or (null pos) (null edo)))
@@ -109,3 +94,12 @@ solucion=((mov padre) (mov padre) ...)
     (incf (nth (- (cadr pos0) 1) (nth (- (car pos0) 1) var)) aux)
     (decf (nth (- (cadr posD) 1) (nth (- (car posD) 1) var)) aux)
     var)
+
+;algunas pruebas
+(setq edoP '((1 2 3)(4 0 6)(7 5 8)) edo (copy-tree edoP))
+(setq nodo1 '(0 nil 0 0 nil ((1 2 3)(4 0 5)(6 7 8))) nodo2 '(1 0 2 1 2 ((1 2 3)(0 4 5)(6 7 8))) nodo3 '(2 1 3 2 3 ((1 2 0)(3 4 5)(6 7 8))))
+(setq cerrado (list nodo1 nodo2 nodo3))
+(setq solucion (list (list (car (cddddr mejorNodo)) (cadr mejorNodo))))
+(setq mejorNodo '(3 1 3 2 4 ((1 2 0)(3 4 5)(6 7 8))))
+(setq abierto (list nodo1))
+(setq movs '((1 0)(-1 0)(0 1)(0 -1)))
